3f126dd0da68bd4d4638fa799ccdf35de68d3845
MinhajulAkib <00akibswe@gmail.com>
2021-09-25 19:49:37 +0600

    cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,11 +7,19 @@ const Cart = (props) => {
     for(const product of cart){
         total = total + product.price;
     }
+
+    const shipping = total > 0 ? 15: 0;
+    const tax = (total + shipping) * 0.10;
+    const grandTotal = total + shipping + tax;
     return (
         <div>
             <h3>Order Summary</h3>
             <h5>Items Ordered: {props.cart.length}</h5>
             <p> Total: {total}</p>
+            <p> Shipping: {shipping}</p>
+            <p> Tax: {tax}</p>
+            <h3>GrandTotal: {grandTotal}</h3>
+
         </div>
     );
 };
